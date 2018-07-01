0a0615149bf5785c39482f2fe5dc457c0f6946d7
baderk <baderkanawati@gmail.com>
2018-07-01 02:12:21 -0300

    I can get how many times a keyword has occured in a title

--- getTitles.py
+++ getTitles.py
@@ -1,12 +1,32 @@
 import praw
+from collections import Counter
 
-reddit = praw.Reddit(client_id=' jqfZmo_jVWjklA',
-                     client_secret='uHDoZT20IdvspiLymkQ7vjyS8C8',
+# Script
+reddit = praw.Reddit(client_id='5QRK4vEHkC2uhw',
+                     client_secret='TPkY8jF3L5gf1lH1mw-tIgXUZuY',
+                     user_agent='my user agent',
                      username='Ambitious_Vegetable',
                      password='redditAPI_'
-                     user_agent='my user agent')
+                    )
 
-print(reddit.read_only)
 
-for submission in reddit.subreddit('analog').hot(limit=10):
-	print(submission.title)
+
+camera_name = input("Enter camera name: ")
+
+temp_title = ""
+count = 0
+# so  i don't need to create a list and add the title to it. 
+# I could just use the temp_title string and set it to be equal as the submission.title
+for submission in reddit.subreddit('analog').hot(limit=26):
+	# postTitle.append(submission.title)
+	temp_title = submission.title
+	if camera_name in temp_title.lower():
+		count += 1
+print(count)
+
+	# i dont actually need to print the list, but for now it is printed and formatted
+# print ('\n'.join('{}: {}'.format(*k) for k in enumerate(postTitle)))
+# print (postTitle)
+
+
+
